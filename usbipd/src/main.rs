0eb2a5f315c6499bd9e9860a3c3a710033054edd
Adrian Wowk <dev@adrianwowk.com>
2025-11-16 23:42:58 -0600

    daemon cli args

--- usbipd/src/main.rs
+++ usbipd/src/main.rs
@@ -1,3 +1,38 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Debug, clap::Parser)]
+struct Args {
+    /// Bind to IPv4. Default is both
+    #[arg(short = '4', long)]
+    ipv4: bool,
+    /// Bind to IPv6. Default is both
+    #[arg(short = '6', long)]
+    ipv6: bool,
+    /// Run in device mode
+    ///
+    /// Rather than drive an attached device, create a virtual UDC to bind gadgets to
+    #[arg(short = 'e', long)]
+    device: bool,
+    /// Run as a daemon process
+    #[arg(short = 'D', long)]
+    daemon: bool,
+    /// Print debugging information
+    #[arg(short = 'd', long)]
+    debug: bool,
+    /// Write process id to FILE
+    ///
+    /// If no FILE specified, use `/var/run/usbipd.pid`
+    #[arg(short = 'P', long = "pid", name = "FILE")]
+    pid_file: Option<Option<PathBuf>>,
+    /// Listen on TCP/IP port PORT
+    #[arg(short = 't', long = "tcp-port")]
+    port: Option<u16>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    todo!("{args:?}");
 }
